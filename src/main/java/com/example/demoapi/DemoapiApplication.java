25797d70bd370e1d1e6fb76859134a1209512b33
huytqth2008010 <70880121+huytqth2008010@users.noreply.github.com>
2022-09-06 12:41:15 +0700

    Initial commit

---
+++ src/main/java/com/example/demoapi/DemoapiApplication.java
@@ -0,0 +1,33 @@
+package com.example.demoapi;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+public class DemoapiApplication {
+    @Autowired
+    private EmployeeRepository repository;
+
+    @PostMapping("/employee")
+    public Employee addEmployee(@RequestBody Employee employee) {
+        return repository.save(employee);
+    }
+
+    @GetMapping("/employees")
+    public List<Employee> getEmployees() {
+        return repository.findAll();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoapiApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/demoapi/Employee.java
@@ -0,0 +1,27 @@
+package com.example.demoapi;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Getter
+@Entity
+@Table
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String name;
+    private String dept;
+    private long salary;
+}
